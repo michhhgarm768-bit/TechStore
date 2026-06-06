ec7de9d30331debb1d54d41d1f64d3326f8c4c10
Michelle Garcia <saramichel@192.168.1.9>
2026-06-05 21:55:06 -0700

    Ruta GET usuarios

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -6,13 +6,14 @@ router.get('/', async (req, res) => {
 
         res.json(usuarios);
 
-    } catch(error) {
+    } catch (error) {
 
         console.error(error);
 
         res.status(500).json({
             error: error.message
         });
+
     }
 
 });
